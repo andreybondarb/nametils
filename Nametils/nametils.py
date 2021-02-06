ec90f8163d84bfc35919f4fd72661ac5d1261dc3
andreybondarb <53795493+andreybondarb@users.noreply.github.com>
2021-02-07 01:25:36 +0300

    Add files via upload

---
+++ Nametils/nametils.py
@@ -0,0 +1,13 @@
+##
+from input_words import input_word
+from frequencies_count import maximum_word_count
+from predict import predict_personal_names
+
+
+class name_classifier():
+    
+    def predict_names(self, per_name):
+        predictions = predict_personal_names(maximum_word_count(input_word(per_name)))
+        return predictions
+    
+    
