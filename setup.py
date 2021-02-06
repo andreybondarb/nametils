0d6822302892177dca8eddc3ec7c9db666be00a9
andreybondarb <53795493+andreybondarb@users.noreply.github.com>
2021-02-07 01:29:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
+    name='Nametils',
+    version='0.0.1',
     author='Andrey Bondar',
     author_email='andreybondar@yahoo.com',
     description="Bag-of-words classificator of russian personal names.",
     license='MIT',
-    name='Nametils',
     python_requires='>=3.5.0',
-    install_requires=[
-        'pytils>=0.3'],
-    packages=find_packages(exclude=['examples']),
-    url='https://github.com/andreybondarb/Nametils',
-    version='0.0.1',
+    install_requires=['pytils>=0.3'],
+    packages=['Nametils'],
+    url='https://github.com/andreybondarb/Nametils'
 ) 
