7cd82ce2442dbb2a0b147d1d121ac9e547073f4a
andreybondarb <53795493+andreybondarb@users.noreply.github.com>
2021-02-07 12:05:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='Nametils',
+    name='nametils',
     version='0.0.1',
     author='Andrey Bondar',
     author_email='andreybondar@yahoo.com',
@@ -11,6 +11,6 @@ setup(
     license='MIT',
     python_requires='>=3.5.0',
     install_requires=['pytils>=0.3'],
-    packages=['Nametils'],
-    url='https://github.com/andreybondarb/Nametils'
+    packages=['nametils'],
+    url='https://github.com/andreybondarb/nametils'
 ) 
